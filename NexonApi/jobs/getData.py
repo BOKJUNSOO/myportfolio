18d2908bcc17c6be4981b77dba453c828b67997e
BOKJUNSOO <brianbok97@gmail.com>
2024-08-04 20:51:04 +0900

    marsking

--- NexonApi/jobs/getData.py
+++ NexonApi/jobs/getData.py
@@ -1,14 +1,17 @@
-# get reset potential with cube
+# get data
 import requests
 import json
 headers = {
-    "x-nxopen-api-key": "test"
+    "x-nxopen-api-key": "My api key"
 }
 
 date = "2024-05-05"
-urlString = f"https://open.api.nexon.com/maplestory/v1/history/cube?count=1000&date={date}"
+urlString = f"https://open.api.nexon.com/maplestory/v1/history/cube?count=1000&date={date}" 
+# urlString = f"https://open.api.nexon.com/maplestory/v1/history/potential?count=1000&date={date}"
+
 response = requests.get(urlString, headers = headers)
 myapi = response.json()
+
 # save_path and file_name
 file_path = f'C:/Users/brian/Desktop/JUNSOO/Project/NexonApi/data/myapi_c_{date}.json'
 
